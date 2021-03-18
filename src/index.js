cd0f7a3c5b6543ed7ea2949184080c47450cd067
Yana <kornyanakorny@gmail.com>
2021-03-18 17:31:06 +0300

    архивирует файлы

--- src/index.js
+++ src/index.js
@@ -1,41 +1,68 @@
 const fs = require("fs");
+const path = require("path");
 const archiver = require("archiver");
 const config = require("./config/index");
 const { createClient } = require("webdav");
-const output = fs.createWriteStream(__dirname + "/example.zip");
+const output = fs.createWriteStream("files.zip");
 const archive = archiver("zip", {
   zlib: { level: 9 },
 });
+let pathDirectory = config.NASA_SERVICE_URL;
+const client = createClient(pathDirectory, {
+  username: config.NASA_USERNAME,
+  password: config.NASA_PASSWORD,
+});
 async function getDirectory() {
-  return new Promise((resolve) => {
-    const client = createClient(config.NASA_SERVICE_URL, {
-      username: config.NASA_USERNAME,
-      password: config.NASA_PASSWORD,
-    });
-    const directoryItems = client.getDirectoryContents("/");
-    resolve(directoryItems);
+  return new Promise(async (resolve) => {
+    const directory = await client.getDirectoryContents("/");
+    resolve(directory);
   });
 }
-async function getDirectoryItem() {
-  const p = await getDirectory();
-  for (i = 0; i < p.length; i++) {
-    if (p[i].type === "directory") {
-      archive.directory(p[i].basename, p[i].basename);
-      archive.pipe(output);
+// async function saveDirectory(name) {
+//   fs.mkdir(path.join(__dirname + "/archive", `${name}`), (err) => {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     console.log("Directory created successfully!");
+//   });
+// }
+async function getContents(name) {
+  const file = await client.getFileContents(name, { format: "text" });
+  return file;
+}
+async function getDirectoryContents() {
+  const directoryContents = await getDirectory();
+  for (i = 0; i < directoryContents.length; i++) {
+    switch (directoryContents[i].type) {
+      case "directory": {
+        // pathDirectory = pathDirectory + directoryContents[i].filename;
+        // await getDirectory();
+        // await saveDirectory(directoryContents[i].basename);
+        // console.log(pathDirectory);
+        break;
+      }
+      case "file": {
+        const fileContents = await getContents(directoryContents[i].basename);
+        archive.append(`${fileContents}`, {
+          name: directoryContents[i].filename,
+        });
+        break;
+      }
+      default: {
+        console.log("Ошибка");
+        break;
+      }
     }
   }
 }
-
 (async () => {
+  await getDirectoryContents();
+  await archive.pipe(output);
+  archive.finalize();
   output.on("close", function () {
     console.log(archive.pointer() + " total bytes");
     console.log(
       "archiver has been finalized and the output file descriptor has closed."
     );
   });
-  await getDirectoryItem();
-  output.on("end", function () {
-    console.log("Data has been drained");
-  });
-  archive.finalize();
 })();

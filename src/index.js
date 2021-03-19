5dd0e11b392a015f9aa439dfa0ba0546c64d1be3
Yana <kornyanakorny@gmail.com>
2021-03-19 16:01:21 +0300

    сохраняет структуру папок

--- src/index.js
+++ src/index.js
@@ -1,50 +1,65 @@
 const fs = require("fs");
 const path = require("path");
 const archiver = require("archiver");
-const config = require("./config/index");
+const { NASA_SERVICE_URL } = require("./config/index");
+const { NASA_USERNAME } = require("./config/index");
+const { NASA_PASSWORD } = require("./config/index");
 const { createClient } = require("webdav");
 const output = fs.createWriteStream("files.zip");
 const archive = archiver("zip", {
   zlib: { level: 9 },
 });
-let pathDirectory = config.NASA_SERVICE_URL;
-const client = createClient(pathDirectory, {
-  username: config.NASA_USERNAME,
-  password: config.NASA_PASSWORD,
+const client = createClient(NASA_SERVICE_URL, {
+  username: NASA_USERNAME,
+  password: NASA_PASSWORD,
 });
-async function getDirectory() {
+async function getDirectory(name) {
   return new Promise(async (resolve) => {
-    const directory = await client.getDirectoryContents("/");
+    const directory = await client.getDirectoryContents(name);
     resolve(directory);
   });
 }
-// async function saveDirectory(name) {
-//   fs.mkdir(path.join(__dirname + "/archive", `${name}`), (err) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     console.log("Directory created successfully!");
-//   });
-// }
+async function saveDirectory(name) {
+  fs.access(__dirname + "/archive" + name, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.mkdir(path.join(__dirname + "/archive/", `${name}`), (err) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("Directory created successfully!");
+      });
+    } else {
+      return;
+    }
+  });
+}
 async function getContents(name) {
   const file = await client.getFileContents(name, { format: "text" });
   return file;
 }
-async function getDirectoryContents() {
-  const directoryContents = await getDirectory();
+async function goInsideTheDirectory(name) {
+  const directoryContents = await getDirectory(name);
   for (i = 0; i < directoryContents.length; i++) {
+    await saveDirectory(directoryContents[i].filename);
+    await getDirectoryContents(directoryContents[i].filename);
+  }
+}
+async function getDirectoryContents(name) {
+  const directoryContents = await getDirectory(name);
+  for (i = 0; i < directoryContents.length; i++) {
+    if (!directoryContents[i].type.includes("directory")) {
+      await getDirectoryContents("/");
+    }
     switch (directoryContents[i].type) {
       case "directory": {
-        // pathDirectory = pathDirectory + directoryContents[i].filename;
-        // await getDirectory();
-        // await saveDirectory(directoryContents[i].basename);
-        // console.log(pathDirectory);
+        await saveDirectory(directoryContents[i].filename);
+        await goInsideTheDirectory(directoryContents[i].filename);
         break;
       }
       case "file": {
-        const fileContents = await getContents(directoryContents[i].basename);
+        const fileContents = await getContents(directoryContents[i].filename);
         archive.append(`${fileContents}`, {
-          name: directoryContents[i].filename,
+          name: directoryContents[i].basename,
         });
         break;
       }
@@ -56,7 +71,7 @@ async function getDirectoryContents() {
   }
 }
 (async () => {
-  await getDirectoryContents();
+  await getDirectoryContents("");
   await archive.pipe(output);
   archive.finalize();
   output.on("close", function () {
